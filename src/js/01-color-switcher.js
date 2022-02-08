505202096989e05d43b8f50988eb7b190baa5c3e
Lilia Lomaka <llomaka80@gmail.com>
2022-02-08 21:01:25 +0200

    Завдання 1 виконано

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+const getRandomHexColor = function () {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeBodyColor = function () {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+const onButtonsClick = function (event) {
+  if (event.target.nodeName !== 'BUTTON') return;
+  if (event.target.dataset.hasOwnProperty('start')) {
+    refs.stopButton.disabled = false;
+    refs.startButton.disabled = true;
+    refs.interval = setInterval(changeBodyColor, 1000);
+    return;
+  }
+  if (event.target.dataset.hasOwnProperty('stop')) {
+    refs.stopButton.disabled = true;
+    refs.startButton.disabled = false;
+    clearInterval(refs.interval);
+    refs.body.style.backgroundColor = 'initial';
+    return;
+  }
+};
+
+refs.stopButton.disabled = true;
+refs.body.addEventListener('click', onButtonsClick);
