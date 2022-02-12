0a5102b946c12f8ceaaa8d515bc6d8401f6c5ae5
Lilia Lomaka <llomaka80@gmail.com>
2022-02-13 00:04:42 +0200

    Виконання 3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.forms[0],
+  delay: document.forms[0].delay,
+  step: document.forms[0].step,
+  amount: document.forms[0].amount,
+  button: document.querySelector('button'),
+}
+
+refs.button.addEventListener('click', onClick);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +17,14 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
+}
+
+function onClick(event) {
+  event.preventDefault();
+  if (refs.delay.value < 0 || refs.step.value < 0 || refs.amount.value < 0) {
+    return Notify.failure('Enter positive values in mandatory fields: First delay, Delay step and Amount');
+  }
+  console.log(refs.delay.value, refs.step.value, refs.amount.value);
+
+  refs.form.reset();
 }
